9bc27e628996283246677e630d3a2fcf6d1d6367
Miami Crypto Labs <105951495+MiamiCrypto@users.noreply.github.com>
2024-06-10 14:39:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 # Set the page configuration for dark mode
 #st.set_page_config(page_title="Salary Prediction Dashboard", page_icon="💼", layout="wide", initial_sidebar_state="expanded")
 #st.set_page_config(page_title="Salary Prediction Dashboard", page_icon="💼", layout="wide", initial_sidebar_state="collapsed")
-st.set_page_config(page_title="Salary Prediction Dashboard", page_icon="💼", layout="wide", initial_sidebar_state="auto")
+st.set_page_config(page_title="Salary Prediction Dashboard", page_icon="💼", layout="centered", initial_sidebar_state="auto")
 
 # Custom CSS to hide the "Made with Streamlit" watermark
 hide_streamlit_style = """
