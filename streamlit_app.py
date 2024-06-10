d4c10cc65d1dd6b93b51c4982a3155c24835cc5b
Miami Crypto Labs <105951495+MiamiCrypto@users.noreply.github.com>
2024-06-10 18:50:41 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,8 +84,8 @@ col1, col2, col3= st.columns(3)
 # Display the image smaller and centered
 
 with col2 = 
-st.image("salaryprediction.png", width=300, caption="Predict your future Salary")
-st.markdown("</div>", unsafe_allow_html=True)
+    st.image("salaryprediction.png", width=300, caption="Predict your future Salary")
+    st.markdown("</div>", unsafe_allow_html=True)
 
 # Display the image smaller and centered
 #st.image("salaryprediction.png", width=300, caption="Predict your future Salary")
