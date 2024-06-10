269cd6effd8806515fe3bf031c7159830e922ad5
Miami Crypto Labs <105951495+MiamiCrypto@users.noreply.github.com>
2024-06-10 14:14:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -139,11 +139,12 @@ with col2:
     ax.set_ylabel('Frequency')
     st.pyplot(fig)
 
-    st.subheader("Average Salary by Major")
+    st.header("Average Salary by Major")
     avg_salary_by_major = students_data.groupby('Major')['Salary'].mean().reset_index()
     
 # Debugging: Print the DataFrame to ensure it looks correct
 st.write("Average Salary by Major DataFrame:")
+#st.header("Average Salary by Major")
 st.write(avg_salary_by_major)
     
 if not avg_salary_by_major.empty:
